1d005ec176f8e510f977aecb26f2022db2cef7bc
BERREDJEM DEL-VALLE Jorys <jberredj@student.42.fr>
2021-01-26 14:18:27 +0100

    Change in behavior if width is negative

--- srcs/check_parser/check_parser.c
+++ srcs/check_parser/check_parser.c
@@ -6,7 +6,7 @@
 /*   By: jberredj <jberredj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/05 11:53:12 by jberredj          #+#    #+#             */
-/*   Updated: 2021/01/26 14:14:08 by jberredj         ###   ########.fr       */
+/*   Updated: 2021/01/26 14:17:52 by jberredj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,10 @@ int	check_parser(t_pf *flags)
 	{
 		flags->width = va_arg(*(flags->ap), int);
 		if (flags->width < 0)
-			flags->width_state = NOT_SET;
+		{
+			flags->flags & MINUS_FLAG;
+			flags->precision = - flags->precision;
+		}
 		else
 			flags->width_state = SET;
 	}
