ae1538da163c4cf127445ee1e1b55804e224e2a3
BERREDJEM DEL-VALLE Jorys <jberredj@student.42.fr>
2021-01-26 16:06:39 +0100

    svp montre moi ce qui est casse

--- srcs/check_parser/check_parser.c
+++ srcs/check_parser/check_parser.c
@@ -6,7 +6,7 @@
 /*   By: jberredj <jberredj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/05 11:53:12 by jberredj          #+#    #+#             */
-/*   Updated: 2021/01/26 15:11:46 by jberredj         ###   ########.fr       */
+/*   Updated: 2021/01/26 16:05:58 by jberredj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,10 +45,7 @@ int	check_parser(t_pf *flags)
 		flags->width = va_arg(*(flags->ap), int);
 		flags->width_state = SET;
 		if (flags->width < 0)
-		{
 			flags->flags |= MINUS_FLAG;
-			flags->width = - flags->width;
-		}
 	}
 	if (flags->precision_state == VAR_VALUE)
 	{
--- srcs/check_parser/p_type_illegal.c
+++ srcs/check_parser/p_type_illegal.c
@@ -6,7 +6,7 @@
 /*   By: jberredj <jberredj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/05 14:26:04 by jberredj          #+#    #+#             */
-/*   Updated: 2021/01/26 15:31:56 by jberredj         ###   ########.fr       */
+/*   Updated: 2021/01/26 15:38:26 by jberredj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,5 @@ int	p_type_illegal(t_pf *flags)
 	flags->flags &= ~ HASH_FLAG;
 	flags->flags &= ~ PLUS_FLAG;
 	flags->flags &= ~ SPACE_FLAG;
-	/*if (flags->precision_state == VAR_VALUE)
-		(void)va_arg(*(flags->ap), int);
-	flags->precision = 0;
-	flags->precision_state = NOT_SET;*/
 	return (0);
 }
