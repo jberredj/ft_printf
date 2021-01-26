9637580cd3488a6ba85d5863b0385118b489540f
BERREDJEM DEL-VALLE Jorys <jberredj@student.42.fr>
2021-01-26 16:13:57 +0100

    try to correct

--- srcs/converter/process_width.c
+++ srcs/converter/process_width.c
@@ -6,11 +6,20 @@
 /*   By: jberredj <jberredj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/21 17:13:02 by jberredj          #+#    #+#             */
-/*   Updated: 2021/01/24 00:27:43 by jberredj         ###   ########.fr       */
+/*   Updated: 2021/01/26 16:13:39 by jberredj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+int	check_width(t_pf *flags)
+{
+	if (flags->width_state == NOT_SET)
+		return (0);
+	if (flags->flags & MINUS_FLAG)
+		return (- flags->width);
+	return(flags->width);
+}
 
 char	*process_width(char *str, size_t *len, t_pf *flags)
 {
@@ -19,9 +28,9 @@ char	*process_width(char *str, size_t *len, t_pf *flags)
 
 	if (str == NULL)
 		return (NULL);
-	if (flags->width_state == NOT_SET || flags->width <= 0)
+	width = check_width(flags);
+	if (width <= 0)
 		return (str);
-	width = flags->width;
 	tmp = ft_calloc(width + *len + 1, sizeof(char));
 	if (!tmp)
 		return (NULL);
