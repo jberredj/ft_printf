bfa680c650521fc4e26b7555b5470c09aab74f29
BERREDJEM DEL-VALLE Jorys <jberredj@student.42.fr>
2021-01-27 15:39:53 +0100

    Change Includes

--- includes/parser.h
+++ includes/parser.h
@@ -6,7 +6,7 @@
 /*   By: jberredj <jberredj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/04 19:08:51 by jberredj          #+#    #+#             */
-/*   Updated: 2021/01/27 15:05:19 by jberredj         ###   ########.fr       */
+/*   Updated: 2021/01/27 15:39:23 by jberredj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 # include "parser_flag.h"
 # include "parser_length.h"
 # include "parser_type.h"
-# include "../libft/includes/ft_to.h"
+# include "ft_printf.h"
 # define NOT_SET 2
 # define SET 1
 # define VAR_VALUE 4
